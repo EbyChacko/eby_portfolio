e7967dff5f6df5bbeb3136065c48e3f1c37f7f02
Eby <eby@MAC.station>
2024-10-14 17:51:40 +0100

    Fix the error in starting front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "frontend/src/components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar/>
     </div>
   );
 }
